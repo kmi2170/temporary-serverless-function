4f11388fbe78d973dcd9b6d19d1fab553c653f18
kmi <sageglorieux@hotmail.com>
2022-04-18 14:34:04 -0700

    the first functons: 1-hello

---
+++ .netlify/functions-serve/1-hello/src/functions/1-hello.js
@@ -0,0 +1,23 @@
+var __defProp = Object.defineProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __export = (target, all) => {
+  __markAsModule(target);
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+
+// functions/1-hello.ts
+__export(exports, {
+  handler: () => handler
+});
+var handler = async (event, context, cb) => {
+  return {
+    statusCode: 200,
+    body: "Our First Netlify Function Example"
+  };
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  handler
+});
+//# sourceMappingURL=1-hello.js.map
